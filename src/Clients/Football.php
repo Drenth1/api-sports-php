d55a2e263ee2e8e78edf0a32ae59baa53f6c2f02
Joey Drenth <drenthjoey@gmail.com>
2023-05-28 19:20:10 +0200

    add functions to query the leagues endpoint

--- src/Clients/Football.php
+++ src/Clients/Football.php
@@ -4,12 +4,14 @@ namespace Drenth1\ApiSports\Clients;
 
 use Drenth1\ApiSports\Core\Client;
 use Drenth1\ApiSports\Endpoints\Shared\Sets\SharedEndpoints;
+use Drenth1\ApiSports\Endpoints\Football\Sets\LeagueEndpoints;
 use Drenth1\ApiSports\Endpoints\Football\Sets\CountryEndpoints;
 use Drenth1\ApiSports\Endpoints\Football\Sets\TimezoneEndpoints;
 
 class Football extends Client
 {
     use SharedEndpoints;
+    use LeagueEndpoints;
     use CountryEndpoints;
     use TimezoneEndpoints;
 }
