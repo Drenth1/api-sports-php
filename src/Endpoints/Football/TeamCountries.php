6d8f0ef921258e25a7d30f7e6085b7ff390b9788
Joey Drenth <drenthjoey@gmail.com>
2023-05-29 14:30:38 +0200

    add endpoint classes for team related endpoints

---
+++ src/Endpoints/Football/TeamCountries.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drenth1\ApiSports\Endpoints\Football;
+
+use Drenth1\ApiSports\Core\Endpoint;
+
+class TeamCountries extends Endpoint
+{
+    /**
+     * Get an array of possible request parameters for the Endpoint.
+     *
+     * @param string $version the version of the api to use.
+     * @return array
+     */
+    public static function parameters(string $version) : array
+    {
+        return [];
+    }
+
+    /**
+     * Get the URL-path for the Endpoint.
+     *
+     * @param string $version the version of the api to use.
+     * @return string
+     */
+    public static function path(string $version) : string
+    {
+        return 'teams/countries';
+    }
+}
