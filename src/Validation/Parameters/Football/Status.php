06800964d73494dbb80de240e63e94deccfa1d31
Joey Drenth <drenthjoey@gmail.com>
2023-05-30 10:11:35 +0200

    add parameters for football

---
+++ src/Validation/Parameters/Football/Status.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drenth1\ApiSports\Validation\Parameters\Football;
+
+use Drenth1\ApiSports\Validation\Parameters\Parameter;
+
+class Status extends Parameter
+{
+    /**
+     * The rules that the Parameter should comply with.
+     *
+     * @var array[]
+     */
+    protected static array $rules = [
+        'dataType' => ['string'],
+    ];
+}
