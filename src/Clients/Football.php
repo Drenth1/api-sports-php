8d52398e82ce62c6e549edb02289caa1c1fc02b9
Joey Drenth <drenthjoey@gmail.com>
2023-06-16 14:00:44 +0200

    add parameters

--- src/Clients/Football.php
+++ src/Clients/Football.php
@@ -3,25 +3,9 @@
 namespace Drenth1\ApiSports\Clients;
 
 use Drenth1\ApiSports\Core\Client;
-use Drenth1\ApiSports\Endpoints\Shared\Sets\SharedEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\TeamEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\VenueEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\LeagueEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\SeasonEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\FixtureEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\CountryEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\TimezoneEndpoints;
-use Drenth1\ApiSports\Endpoints\Football\Sets\StandingEndpoints;
+use Drenth1\ApiSports\Core\Methods\Generic\FetchesGenericEndpoints;
 
 class Football extends Client
 {
-    use TeamEndpoints;
-    use VenueEndpoints;
-    use SharedEndpoints;
-    use LeagueEndpoints;
-    use SeasonEndpoints;
-    use FixtureEndpoints;
-    use CountryEndpoints;
-    use TimezoneEndpoints;
-    use StandingEndpoints;
+    use FetchesGenericEndpoints;
 }
