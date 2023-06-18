f7cdfff46f9549045e17805f6cef85195e0de7c4
Joey Drenth <drenthjoey@gmail.com>
2023-06-18 10:13:09 +0200

    add venue endpoints

--- src/Clients/Football.php
+++ src/Clients/Football.php
@@ -4,6 +4,7 @@ namespace Drenth1\ApiSports\Clients;
 
 use Drenth1\ApiSports\Core\Client;
 use Drenth1\ApiSports\Core\Methods\Football\FetchesTeamEndpoints;
+use Drenth1\ApiSports\Core\Methods\Football\FetchesVenueEndpoints;
 use Drenth1\ApiSports\Core\Methods\Football\FetchesSeasonEndpoints;
 use Drenth1\ApiSports\Core\Methods\Generic\FetchesGenericEndpoints;
 use Drenth1\ApiSports\Core\Methods\Football\FetchesLeaguesEndpoints;
@@ -14,6 +15,7 @@ use Drenth1\ApiSports\Core\Methods\Football\FetchesTeamStatisticEndpoints;
 class Football extends Client
 {
     use FetchesTeamEndpoints;
+    use FetchesVenueEndpoints;
     use FetchesSeasonEndpoints;
     use FetchesCountryEndpoints;
     use FetchesGenericEndpoints;
