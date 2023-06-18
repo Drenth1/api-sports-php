2ae4692f566c01ede5ccf7c92cb36710719fd42c
Joey Drenth <drenthjoey@gmail.com>
2023-06-18 11:47:44 +0200

    add fixture lineup type parameter

---
+++ src/Validation/Parameters/Football/FixtureLineupType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drenth1\ApiSports\Validation\Parameters\Football;
+
+use Drenth1\ApiSports\Validation\Parameter;
+
+class FixtureLineupType extends Parameter
+{
+    /**
+     * The rules that the Parameter must pass.
+     *
+     * @var array
+     */
+    protected static array $rules = [
+        'dataType' => ['string'],
+        'enum' => [['Formation', 'Coach', 'Start XI', 'Substitutes']]
+    ];
+}
