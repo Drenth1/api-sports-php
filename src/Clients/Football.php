4c54ed25a5591f77ecabc21c74858c9557a33ccf
Joey Drenth <drenthjoey@gmail.com>
2023-06-18 09:51:56 +0200

    add team endpoints

--- src/Clients/Football.php
+++ src/Clients/Football.php
@@ -3,6 +3,7 @@
 namespace Drenth1\ApiSports\Clients;
 
 use Drenth1\ApiSports\Core\Client;
+use Drenth1\ApiSports\Core\Methods\Football\FetchesTeamEndpoints;
 use Drenth1\ApiSports\Core\Methods\Football\FetchesSeasonEndpoints;
 use Drenth1\ApiSports\Core\Methods\Generic\FetchesGenericEndpoints;
 use Drenth1\ApiSports\Core\Methods\Football\FetchesLeaguesEndpoints;
@@ -11,6 +12,7 @@ use Drenth1\ApiSports\Core\Methods\Football\FetchesTimezoneEndpoints;
 
 class Football extends Client
 {
+    use FetchesTeamEndpoints;
     use FetchesSeasonEndpoints;
     use FetchesCountryEndpoints;
     use FetchesGenericEndpoints;
